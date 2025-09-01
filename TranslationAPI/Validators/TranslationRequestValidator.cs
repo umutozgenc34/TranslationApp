1f5887ffa7576d7d7bdda77da5b737ec0e5e0c73
umutozgenc34 <umutozgenc34@hotmail.com>
2025-09-01 15:40:23 +0300

    refactor 2 - data annotations removed, fluent validation used

---
+++ TranslationAPI/Validators/TranslationRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using TranslationAPI.Models.Requests;
+
+namespace TranslationAPI.Validators;
+
+public class TranslationRequestValidator : AbstractValidator<TranslationRequest>
+{
+    private readonly List<string> _validLanguages = new()
+    { "auto", "tr", "en", "de", "fr", "es", "it" };
+
+    public TranslationRequestValidator()
+    {
+        RuleFor(x => x.Text)
+            .NotEmpty().WithMessage("Çevrilecek metin boş olamaz")
+            .MaximumLength(5000).WithMessage("Metin 5000 karakterden uzun olamaz");
+
+        RuleFor(x => x.FromLanguage)
+            .Must(x => _validLanguages.Contains(x.ToLower()))
+            .WithMessage("Geçersiz kaynak dil kodu");
+
+        RuleFor(x => x.ToLanguage)
+            .NotEmpty().WithMessage("Hedef dil gerekli")
+            .Must(x => _validLanguages.Contains(x.ToLower()))
+            .WithMessage("Geçersiz hedef dil kodu");
+    }
+}
